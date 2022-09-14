01c4eab9e7b9b895716213d176a39c5f0fd4c5f0
ArjunSanthoshpnr <arjunpnr@gmail.com>
2022-09-14 18:42:11 +0530

    replaced link with nav-link router

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,31 +1,10 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledTab = styled.span`
   font-size: 18px;
-  color: ${(props) =>
-    props.active ? props.theme.text : props.theme.btnBgFade};
+  color: ${(props) => props.theme.grayShade200};
   cursor: pointer;
-  display: inline-block;
-  position: relative;
-  line-height: 2.5rem;
-  :after {
-    content: "";
-    position: absolute;
-    width: 100%;
-    transform: scaleX(0);
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: ${(props) => props.theme.text};
-    transform-origin: bottom left;
-    transition: transform 0.25s ease-out;
-    transform: ${(props) => props.active && "scaleX(1)"};
-  }
-  :hover:after {
-    transform: scaleX(1);
-  }
 `;
 
 export const Tab = ({ label, title = "Title", ...props }) => {
@@ -37,23 +16,17 @@ export const Tab = ({ label, title = "Title", ...props }) => {
 };
 
 export const Tabs = (props) => {
-  const [activeTab, setActiveTab] = useState("home");
-
-  const switchTabs = (tab) => {
-    setActiveTab(tab);
-  };
-
   return (
     <Container>
       {props.children.map((tab) => {
         return (
-          <Link key={tab.props.label} to={tab.props.path}>
-            <Tab
-              onClick={switchTabs}
-              active={activeTab === tab.props.label}
-              {...tab.props}
-            />
-          </Link>
+          <NavLink
+            key={tab.props.label}
+            to={tab.props.path}
+            className={({ isActive }) => (isActive ? "active-tab" : undefined)}
+          >
+            <Tab {...tab.props} />
+          </NavLink>
         );
       })}
     </Container>
@@ -64,6 +37,39 @@ const Container = styled.div`
   display: flex;
   gap: 4rem;
   align-items: center;
+  a {
+    display: inline-block;
+    position: relative;
+    line-height: 2.5rem;
+    text-decoration: none;
+
+    :after {
+      content: "";
+      position: absolute;
+      width: 100%;
+      transform: scaleX(0);
+      height: 2px;
+      bottom: 0;
+      left: 0;
+      background-color: ${(props) => props.theme.text};
+      transform-origin: bottom left;
+      transition: transform 0.25s ease-out;
+    }
+    :hover:after {
+      transform: scaleX(1);
+    }
+  }
+  .active-tab {
+    span {
+      color: ${(props) => props.theme.text};
+    }
+    :link {
+      color: ${(props) => props.theme.text};
+    }
+    :after {
+      transform: scaleX(1);
+    }
+  }
   @media screen and (max-width: 1200px) {
     display: none;
   }
